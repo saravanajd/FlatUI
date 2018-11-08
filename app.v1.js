460709cf50c1d5c512eeddd29eae9f50c639d069
Saravanajd <saravana.jd@gmail.com>
2018-11-08 20:04:04 +0530

    Sidemenu dropdown

--- app.v1.js
+++ app.v1.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function(){
-    $(document).on("click", "ul.nav li > a", function () {
+    $(document).on("click", "ul.nav > li > a", function () {
         $('ul.nav > li').removeClass('active');
+        $('ul.nav > li').removeClass('open');
         var $li = $(this).closest('li');
         $li.addClass('active');
         if ($li.hasClass('dropdown')) {
