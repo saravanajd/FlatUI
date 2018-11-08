91260c6bb1016f48cfcdd03837e0c89686e03b15
Saravanajd <saravana.jd@gmail.com>
2018-11-08 20:34:23 +0530

    headerbar

--- app.v1.js
+++ app.v1.js
@@ -2,9 +2,13 @@
 $(document).ready(function(){
     $(document).on("click", "ul.nav > li > a", function () {
         $('ul.nav > li').removeClass('active');
-        $('ul.nav > li').removeClass('open');
+
         var $li = $(this).closest('li');
         $li.addClass('active');
+        if(!$li.hasClass('open')){
+            $('ul.nav > li.dropdown').removeClass('open');
+        }
+        
         if ($li.hasClass('dropdown')) {
             $li.toggleClass('open');
         }
