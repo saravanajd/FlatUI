13cc5d5cd9d5b9533f6e252d04f34a45644f36f0
saravana jd <saravana.jd@gmail.com>
2018-11-29 00:09:08 +0530

    sidenav animation

--- app.v1.js
+++ app.v1.js
@@ -1,15 +1,55 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
+    // Toggle sidenav
+    $(document).on("click", "header .navbar-collopse", function () {
+        $('.nav > li > a > span').toggleClass('hide');
+        $('body').toggleClass('minbar')
+        if ($('body').hasClass('minbar')) {
+            $('.main aside').animate({
+                width: 60
+            }, 500)
+            $('.main').animate({
+                "margin-left": 60
+            }, 500)
+        } else {
+            $('.main aside').animate({
+                width: 220
+            }, 500)
+            $('.main').animate({
+                "margin-left": 220
+            }, 500)
+        }
+        // $('.main aside').toggleClass('w-50');
+
+    });
+
+    // Sidenav toggle active and open
     $(document).on("click", "ul.nav > li > a", function () {
         $('ul.nav > li').removeClass('active');
 
         var $li = $(this).closest('li');
         $li.addClass('active');
-        if(!$li.hasClass('open')){
+        if (!$li.hasClass('open')) {
             $('ul.nav > li.dropdown').removeClass('open');
+            $('ul.nav > li.dropdown ul').animate({
+                opacity: 0,
+                "margin-left": "-5rem"
+            }, 20);
         }
-        
+
         if ($li.hasClass('dropdown')) {
+            if (!$li.hasClass('open')) {
+                $li.find('ul').animate({
+                    opacity: 1,
+                   // height: $li.find('ul').get(0).scrollHeight,
+                    "margin-left": "0"
+                }, 50);
+            } else {
+                $li.find('ul').animate({
+                    opacity: 0,
+                    "margin-left": "-5rem"
+                }, 20);
+            }
             $li.toggleClass('open');
         }
     });
