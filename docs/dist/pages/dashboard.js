e1d9badbf7c504e977fae60f10eef4966b7d363e
Saravanajd <saravana.jd@gmail.com>
2018-12-07 22:05:38 +0530

    mobile view start

--- docs/dist/pages/dashboard.js
+++ docs/dist/pages/dashboard.js
@@ -133,40 +133,6 @@ var config = {
     }
 };
 
-// var config = {
-//     type: 'pie',
-//     data: {
-//         datasets: [{
-//             data: [
-//                 randomScalingFactor(),
-//                 randomScalingFactor(),
-//                 randomScalingFactor(),
-//                 randomScalingFactor(),
-//                 randomScalingFactor(),
-//             ],
-//             backgroundColor: [
-//                 window.chartColors.red,
-//                 window.chartColors.orange,
-//                 window.chartColors.yellow,
-//                 window.chartColors.green,
-//                 window.chartColors.blue,
-//             ],
-//             label: 'Dataset 1'
-//         }],
-//         labels: [
-//             'Red',
-//             'Orange',
-//             'Yellow',
-//             'Green',
-//             'Blue'
-//         ]
-//     },
-//     options: {
-//         responsive: true,
-//         maintainAspectRatio: false
-//     }
-// };
-
 $(document).ready(function () {
     var ctx = document.getElementById('canvas').getContext('2d');
     new Chart(ctx, {
